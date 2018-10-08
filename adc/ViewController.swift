d60aa61ddebaf39d9c3884d5a7cbc6f229572b57
tran duc anh <ducanhxom4@gmail.com>
2018-10-08 16:37:34 +0700

    adc123

--- adc/ViewController.swift
+++ adc/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var arrayNumbers = [Int](1...3)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
